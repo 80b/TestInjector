d40dbcbbf36c07a94453b082f868a8f6cd46c4fa
razberries <54728263+razberries@users.noreply.github.com>
2020-11-12 17:51:10 -0500

    Update Build.cpp

--- DLL/Build.cpp
+++ DLL/Build.cpp
@@ -1,25 +1,6 @@
 #include <windows.h>
 
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD u1_reason_for_call,
-                       LPVOID lpReserved
-                    )
-{
-    switch(u1_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        MessageBox(NULL, "Injected!", "Success", NULL); //lol oops retard moment
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
-}
-#include <windows.h>
-
-
 BOOL APIENTRY DllMain(HMODULE hModule,
     DWORD u1_reason_for_call,
     LPVOID lpReserved
